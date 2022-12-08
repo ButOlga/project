da95974fecaf59ec4a99566475551c0d5dc73486
Olga <buttieva@yandex.ru>
2022-12-08 22:33:26 +0300

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+'use srtict';
+let arr = ['plum', 'orange', 'apple'];
+
+// alert("Hello, world!");
+
+// let ans = +prompt("Сколько Вам лет?", "");
+
+// console.log(typeof(ans));
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(5%2);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked && !isClosed);

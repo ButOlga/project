f619defd4fe85247ed76836ccd64d05d4b4408bd
Olga <buttieva@yandex.ru>
2022-12-11 18:29:44 +0300

    circles_2

--- script.js
+++ script.js
@@ -23,6 +23,20 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//         b = prompt("Во сколько это обойдется?", '');
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b !='' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('bad result');
+//         i--;
+//     }
+//     i++;
+// }
+
 appData.moneyPerDay = appData.budget / 30;
 
 alert("Ежедневный бюджет " + appData.moneyPerDay);

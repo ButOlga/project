2570483fd7fb178b0a2c238a90407e37f9f2139c
Olga <buttieva@yandex.ru>
2022-12-11 14:19:20 +0300

    circles

---
+++ js.js
@@ -0,0 +1,35 @@
+// switch (num) {
+//     case num < 49:
+//         console.log("NO right");
+//         break;
+//     case num > 100:
+//         console.log("Too much!");
+//         break;
+//     case 50:
+//         console.log("Right!");
+//         break;
+//         default:
+//             console.log('error');
+//             break;
+// }
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 4) {
+        continue;
+    }
+    console.log(i);
+}
+
